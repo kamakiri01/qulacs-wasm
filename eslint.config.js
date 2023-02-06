42308293cfb50f130d6375de852bb86cb3df3897
kamakiri01 <kamakiri01@users.noreply.github.com>
2023-02-07 03:48:34 +0900

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,34 @@
+// using Flat Config
+
 const tsPlugin = require("@typescript-eslint/eslint-plugin");
 const tsParser = require("@typescript-eslint/parser");
+const globals = require("globals");
 
 module.exports = [
     "eslint:recommended",
     {
+        rules: {
+            "no-redeclare": "off"
+        }
+    },
+    {
         files: ["src-ts/**/*.ts"],
         languageOptions: {
-          parser: tsParser  
+          parser: tsParser,
+          globals: {
+            ...globals.node,
+            "WebAssembly": false,
+            "EmscriptenWasm": false
+            }
         },
         plugins: {
             "@typescript-eslint": tsPlugin
         },
         rules: {
             ...tsPlugin.configs["recommended"].rules,
-            ...tsPlugin.configs["eslint-recommended"].rules
-        }  
-    },
-    {
-        rules: {
-            "no-redeclare": "off"
-        }
+            ...tsPlugin.configs["eslint-recommended"].rules,
+            "@typescript-eslint/no-empty-interface": "off",
+            "@typescript-eslint/no-var-requires": "off"
+        },
     }
 ];
